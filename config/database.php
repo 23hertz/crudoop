dfb8e9c61b8bb4718bf50e1b44c22278111a7b53
23hertz <57693962+23hertz@users.noreply.github.com>
2021-03-18 23:08:24 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$db_name = "crud_oop";
-$username = "root";
-$password = "sark";
+$host = "xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$db_name = "u56z3i0lke85iigj";
+$username = "o3qbj4q55et5icd2";
+$password = "cp1g60bf1h8ey8f3";
 
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}",$username,$password);
